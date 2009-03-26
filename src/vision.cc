03b8a97ccb3ff6db7be48fd224724c4a89d0582f
Audun D. Christoffersen <audunc@met.no>
2009-03-26 15:31:08 +0000

    riggvarsel support

--- src/vision.cc
+++ src/vision.cc
@@ -1,6 +1,6 @@
 /*
   libpuMet - Meteorological algorithms (Weather symbol generator etc.)
-  
+
   $Id$
 
   Copyright (C) 2006 met.no
@@ -11,7 +11,7 @@
   0313 OSLO
   NORWAY
   email: diana@met.no
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -21,7 +21,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -29,7 +29,7 @@
 
 
 
-// this little program is ment to be as an translation from the 
+// this little program is ment to be as an translation from the
 // vision index you get from miSymbol. The translation can after
 // intitialising direct be used by miSymbol....
 
@@ -38,18 +38,32 @@
 
 
 vision::vision(){
-
-    AddToList( 1,"0.5", "0-1",   "0-1"               ); 
-    AddToList( 2,"0.5", "0-1",   "@{atTimes} 0-1"    ); 
+/*
+ * // As used in RIGG - removed 26.march 2009
+    AddToList( 1,"0.5", "0-1",   "0-1"               );
+    AddToList( 2,"0.5", "0-1",   "@{atTimes} 0-1"    );
     AddToList( 3,"1",   "0.5-2", "0.5-2"             );
     AddToList( 4,"1",   "0.5-2", "0.5-2 @{inShowers}");
     AddToList( 5,"2",   "1-4",   "1-4"               );
     AddToList( 6,"2",   "1-4",   "1-4 @{inShowers}"  );// new class!
     AddToList( 7,"7",   "4-10",  "4-10"              );
-    AddToList( 8,"7",   "4-10",  "4-10 @{inShowers}" );    
+    AddToList( 8,"7",   "4-10",  "4-10 @{inShowers}" );
     AddToList( 9,"10",  "10",    "@{mainly} 10"      );
     AddToList(10,"10+", "10+",   "10+"               );
-  
+*/
+    AddToList( 1,"0.5", "0-1",   "0-1"               );
+    AddToList( 2,"1",   "0-4",   "0-4"               );
+    AddToList( 3,"1",   "0.5-2", "0.5-2"             );
+    AddToList( 4,"1",   "0.5-2", "0.5-2 @{inShowers}");
+    AddToList( 5,"2",   "1-4",   "1-4"               );
+    AddToList( 6,"2",   "1-4",   "1-4 @{inShowers}"  );
+    AddToList( 7,"6",   "3-8",   "3-8"               );
+    AddToList( 8,"7",   "4-10",  "4-10"              );
+    AddToList( 9,"7",   "4-10",  "4-10 @{inShowers}" );
+    AddToList(10,"8",   "6-10",  "6-10"              );
+    AddToList(11,"10+", "10+",   "10+"               );
+
+
 };
 
 
@@ -80,7 +94,7 @@ int vision::mapToVis(int inp){
     return 2;
   if(inp == 2)
     return 5;
-  return 10;
+  return 11;
 };
 
 
