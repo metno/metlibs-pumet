a82e3d207b589214562de60d8f680bcb1faa13a7
Audun D. Christoffersen <audunc@met.no>
2009-08-27 09:13:26 +0000

    warnings fixed

--- include/symbolMaker.h
+++ include/symbolMaker.h
@@ -1,6 +1,6 @@
 /*
   libpuMet - Meteorological algorithms (Weather symbol generator etc.)
-  
+
   $Id$
 
   Copyright (C) 2006 met.no
@@ -11,7 +11,7 @@
   0313 OSLO
   NORWAY
   email: diana@met.no
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -21,7 +21,7 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-  
+
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -50,14 +50,14 @@
 
 
 
-// this program is used to compute weather symbols from 
+// this program is used to compute weather symbols from
 // cloud, precip and temperature data.
 // The symbols er created by a file, which can be edited.
 // It is possible to change the customer numbers, or the language
 // of the used text. The standart file is:
 // /work/pu/input/weatherSymbols.nor
 // the program checks dynamically the 6 hours before and after
-// the requested termin for data. 
+// the requested termin for data.
 // Because of that it is compatible with ECMWF 6-hours prognosis
 // as well as HIRLAM 3-hours forecasts.
 // JS/PU 7-98
@@ -93,18 +93,18 @@ private:
   paramet cheight;
   paramet ctotal;
   paramet lli;      // lightning index ( 0 / 1 )
-  paramet agr;      // aggregation (0 = water;1 = sleet;2 = snow)    
+  paramet agr;      // aggregation (0 = water;1 = sleet;2 = snow)
   paramet fogi;     // fog-indicator (0 / 1)
   paramet periodOfRain;
   paramet periodOfNoRain;
 
 
-  
+
   static int MAXcustom;
-  static int MINcustom;	
-  static miSymbol sun;                           
-  static miSymbol lightCloud;                      
-  static miSymbol partlyCloud;                     
+  static int MINcustom;
+  static miSymbol sun;
+  static miSymbol lightCloud;
+  static miSymbol partlyCloud;
   static miSymbol cloud;
   static miSymbol fog;
   static miSymbol lightRainSun;
@@ -118,13 +118,13 @@ private:
   static miSymbol RainThunder;
   static miSymbol SnowThunder;
   static miSymbol errorSymbol;
-  
+
   miSymbol myerror;
 public:
   symbolMaker()
     : latitude(60),
       t2m(31),
-      rrf(19), 
+      rrf(19),
       rrk(20),
       rrt(17),
       cs(3901),
@@ -138,18 +138,18 @@ public:
       cmiddle(3903),
       cheight(3904),
       ctotal(25),
+      lli(661),
       agr(170),
       fogi(665),
-      lli(661),
       myerror(errorSymbol) {}
-  
+
   bool initializeModel(vector<paramet>);
   static void readSymbols(miString);
   void periods(vector<miTime>,int,int,bool compute_minmax = false);
 
   /**
    * This function creates a symbol that is not adjusted with repect on
-   * temperature (state of aggregate). The symbol can be adjusted with a 
+   * temperature (state of aggregate). The symbol can be adjusted with a
    * call to stateMaker( miSymbol &symbol_, float temperature,
 	*	                   float stateOfAggregate,
 	*                     float lightningIndex,
@@ -159,7 +159,7 @@ public:
   		                                             const vector<miTime> &termin,
   				      				                     int min = 3,int max = 6);
 
-  
+
   vector<miSymbol> compute(vector<paramet>, vector<miTime>,int,int);
   vector<miSymbol> compute_new(vector<paramet>, vector<miTime>,
 			       int,int,bool compute_minmax = false);
@@ -171,12 +171,12 @@ public:
   bool cloudMaker(miTime);
   bool rainMaker(miTime);
   bool tempMaker(miTime);
-  
+
 
   /**
    * Create an state of aggregate from temperaure.
    * The teperature is in degree Celsius.
-   * 
+   *
    *  0 (snow) temperature <= 0.5.
    *  1 (sleet) 0.5 < temperature < 1.5.
    *  2 (rain)  temeperature >= 1.5
@@ -184,10 +184,10 @@ public:
   static int stateOfAggregateFromTemperature( float temperature );
 
 
-  
+
   /**
    * Change the state of aggregate of the symbol based on the temperature.
-   * 
+   *
    * The change is as follow, only symbols with precipitation is in the table.
    * Other symbols is not affected.
    *
@@ -196,8 +196,8 @@ public:
    *
    * +--------------+-----------------------------------+
    * |              |            state                  |
-   * +--------------+---------+----------+--------------+           
-   * | SYMBOL       |    0    |    1     |     2        |  
+   * +--------------+---------+----------+--------------+
+   * | SYMBOL       |    0    |    1     |     2        |
    * +--------------+---------+----------+--------------+
    * | LIGHTRAINSUN | SNOWSUN | SLEETSUN |     -        |
    * | LIGHTRAIN    | SNOW    | SLEET    |     -        |
@@ -207,11 +207,11 @@ public:
    * | SLEET        | SNOW    |    -     | RAIN         |
    * | SLEETSUN     | SNOWSUN |    -     | LIGHTRAINSUN |
    * +--------------+---------+----------+--------------+
-   * 
+   *
    * If there is lightning the symbol generated fom the table above
    * vil be adjusted as follow.
-   * 
-   * 
+   *
+   *
    * LIGTHRAINSUN -->  LIGHTRAINTHUNDERSUN
    * LIGTHRAIN    -->  LIGHTRAINTHUNDERSUN
    * SLEETSUN     -->  LIGHTRAINTHUNDERSUN
@@ -219,7 +219,7 @@ public:
    * SNOW         -->  SNOWTHUNDER
    * SNOWSUN      -->  SNOWTHUNDER
    * SLEET        -->  SNOWTHUNDER
-   * 
+   *
    * If there is fog, it will override all other symbols and the symbol is set to FOG.
    */
   static bool stateMaker( miSymbol &symbol_,
@@ -227,29 +227,29 @@ public:
 		                    float stateOfAggregate = FLT_MAX,
 	                       float lightningIndex = FLT_MAX,
 	                       float fogIndex = FLT_MAX );
- 
- 
-  
+
+
+
   bool stateMaker( miTime );
- 
+
   /**
    * If there is lightning add lightning to the symbolg. The lightningIndex
    * is a true/false value. Where 0 is false and lightningIndex != 0 is true.
-   * 
-   * If fogIndex is FLT_MAX or FLT_MIN the function does nothing. It is 
+   *
+   * If fogIndex is FLT_MAX or FLT_MIN the function does nothing. It is
    * treated undefined values.
    */
-  
-  static void lightningMaker( miSymbol &symbol_, 
+
+  static void lightningMaker( miSymbol &symbol_,
 		                        float lightningIndex );
 
   bool lightningMaker(miTime);
-  
+
   /**
    * If there is fog set the symbol to fog. The fogIndex
    * is a true/false value. Where 0 is false and fogIndex != 0 is true.
-   * 
-   * If fogIndex is FLT_MAX or FLT_MIN the function does nothing. It is 
+   *
+   * If fogIndex is FLT_MAX or FLT_MIN the function does nothing. It is
    * treated undefined values.
    */
   static void fogMaker( miSymbol &symbol_, float fogIndex);
@@ -262,7 +262,7 @@ public:
   bool isShower(int);
   bool isPrecip(int);
   bool isDry(int);
-  
+
   static int maxCustom() { return MAXcustom;}
   static int minCustom() { return MINcustom;}
   void make_pos_symbols(map<int,int>&, map<int,miString>& );
